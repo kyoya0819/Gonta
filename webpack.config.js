526b66b9d72e1097c75747ba52a0abb55ab7f58a
kyoya0819 <kyoya0819@gmail.com>
2021-01-12 10:45:40 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mode: "development",
+    
+    module: {
+        rules: [
+            {
+                test: /\.scss/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                            importLoaders: 2
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    target: ["web", "es5"],
+};
